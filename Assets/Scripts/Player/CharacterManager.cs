88dde0ee4fb0548b6d6315b4530fb7f7eaa6cdf4
Deepankar Sehgal <deepankarsehgal3@gmail.com>
2026-01-22 16:59:23 +0530

    Completed

---
+++ Assets/Scripts/Player/CharacterManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+using TMPro;
+
+public class CharacterManager : MonoBehaviour
+{
+
+    public int characterIndex;
+ 
+    void Start()
+    {
+        characterIndex = PlayerPrefs.GetInt("SelectedCharacter", 0);
+    }
+}
